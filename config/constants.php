74565ec492bd79a94d9a007324e12600706b8dbe
DoanLe96 <31028561+doanle0906@users.noreply.github.com>
2020-11-03 16:31:23 +0700

    Add files via upload

---
+++ config/constants.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    "PATH_FILE_REPORT_BANK" => "/uploads/report-banks/",
+    "PATH_FILE_LOGO_BANK" => "/uploads/banks/",
+    "TYPE_BANK" => [
+        "LOCAL" => "LOCAL",
+        "GLOBAL" => "GLOBAL"
+    ],
+    "TYPE_VALUE" => [
+        "LOCAL" => "LOCAL",
+        "GLOBAL" => "GLOBAL"
+    ],
+    "COLOR_CHART_WATERWALL" => [
+        "#016FB9",
+        "#CB6952"
+    ],
+    "COLOR_BUBBLE_CHART" => [
+        "#9ACBBF",
+        "#CEE1A5",
+        "#EDEFB8",
+        "#FEE2B4",
+        "#CCCCCC",
+        "#ECBDF2",
+        "#F3BBBF",
+        "#CBC2EF",
+        "#258BC1",
+        "#FF5714"
+    ],
+    "COLOR_BUBBLE_BORDER_CHART" => [
+        "#479980",
+        "#9fc851",
+        "#dce476",
+        "#fcc870",
+        "#ffa736",
+        "#de8ae6",
+        "#ea7a81",
+        "#b6a8ed",
+        "#3c8eba",
+        "#d64c15"
+    ],
+    "COLOR_BUBLE_CHART" => [
+
+    ],
+    "COLOR_BAR_CHART" => "#47997F",
+    "LIST_BANK_DEFAULT" => ["vietcombank", "acbbank", "vietinbank", "sacombank", "bidv", "agribank", "mb"]
+];
